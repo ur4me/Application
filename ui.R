bcb1dad0be798a4aca77d86c8d95c995a1d30f98
Steve Kim <30827345+ur4me@users.noreply.github.com>
2017-10-16 10:25:51 +1300

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,21 +1,10 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
 
-library(shiny)
-
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
   titlePanel("Plot Random Numbers"),
   
-  # Sidebar 
+
   sidebarLayout(
     sidebarPanel(
       numericInput("numeric", "How Many Random Numbers Should be Plotted?",
